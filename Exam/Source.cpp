ecfab9beec1e898044f53a0417eba2837d9b2742
Vita <=>
2022-02-03 14:55:05 +0200

    Registration

--- Exam/Source.cpp
+++ Exam/Source.cpp
@@ -2,12 +2,13 @@
 #include<string>
 #include<map>
 #include<fstream>
+#include<algorithm>
 using namespace std;
 
 int menu() {
 	int log;
 	cout << "1 - Guest ";
-	cout << "2 - Admin ";
+	cout << "\n2 - Admin ";
 	cout << "Enter who are you : ";
 	cin >> log;
 	return log;
@@ -20,23 +21,65 @@ public :
 		this->login = login;
 		this->password = password;
 	}
+	
+	
 };
-class  Login : public Guest {
-	map <int, Guest> mp;
-	Guest g;
-public:
 
 
-
-};
 int main() {
+    string login;
+    string user;
+    int login_times;
+    string password_login;
+    string password;
+    int choice =menu();
+    if (choice == 1) {
+        while (choice) {
+            cout << "\n\n" ;
+            cout << "\t----------------" << " Registeration " << "----------------" << endl;
+            cout << "Login: ";
+            cin >> user;
+            cout << "password: ";
+            cin >> password;
+
+            cout << "\n\n";
+            cout << "Registered Successfully!" << "\n";
+            cout << "\n\n";
+
+            while (choice) {
+                cout << "\n\n";
+                cout << "Login" << "\n" << "_____";
+                cout << "\n";
+
+                cout << "login: ";
+                cin >> login;
+                cout << "Password: ";
+                cin >> password_login;
+
+                if (login == user && password_login == password) {
+                    cout << "loged in Successfully!" << endl;
+                    cout << "Welcome  " << user << "!";
+                    break;
+                }
+                else if (login != user && password_login == password) {
+                    cout << "username is incorrect!" << endl;
+                }
+                else if (login == user && password_login != password) {
+                    cout << "password is incorrect!" << endl;
+                }
+                else {
+                    cout << "Everything is incorrect!" << endl;
+                }
+            }while (choice);
+        }
 
 
 
 
 
 
-	return 0;
+    }
+    return 0;
 }
 
 
