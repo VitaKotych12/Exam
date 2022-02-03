1021c51d96007d721f9839810d97feb00eefe610
Vita <=>
2022-02-03 18:43:53 +0200

    tests

--- Exam/Source.cpp
+++ Exam/Source.cpp
@@ -3,6 +3,7 @@
 #include<map>
 #include<fstream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
 int menu() {
@@ -22,20 +23,7 @@ public :
 		this->login = login;
 		this->password = password;
 	}
-   
-    void add_new() {
-        cout << "\n\n";
-        cout << "\t----------------" << " Registeration " << "----------------" << endl;
-        cout << "Login: ";
-        cin >> user;
-        cout << "password: ";
-        cin >> password;
-
-        cout << "\n\n";
-        cout << "Registered Successfully!" << "\n";
-        cout << "\n\n";
-    }
-	
+ 
     string get_login() {
         return login;
     }
@@ -46,107 +34,129 @@ public :
         return user;
     }
 };
-class login {
-    Guest g;
-    string login;
-    string user;
-    int login_times;
-    string password_login;
-    string password;
-    int choice = menu();
+
+struct Question
+{
+    string question = " ", var_answers = " ";  int answer = 0;
+    bool check_answer(int num)
+    {
+        return (answer == num);
+    }
+};
+class tests {
+    vector<Question> questions;
+    int count_of_questions = 6;
+    int right = 0;
+    int wrong = 0;
+    double res = 0;
+    int mark = 12;
+ public:    
+    void test_programing() {
+
+        questions.resize(count_of_questions);
+        questions[0].question = "\nThere are 6  questions in this test : \n";
+        questions[0].var_answers = "1) C ++ was created in 1979 : \n1 - yes   2 - no\n\n";
+        questions[0].answer = 2;
+
+        questions[1].var_answers = "2) C ++ is object oriented : \n1 - yes   2 - no\n\n";
+        questions[1].answer = 1;
+
+        questions[2].var_answers = "3) how many bytes char contains : \n1 - 1 byte   2 - 8 bytes\n\n";
+        questions[2].answer = 1;
+
+        questions[3].var_answers = "4) how many bytes int contains :  \n1 - 2 bytes   2 - 16 bytes\n\n";
+        questions[3].answer = 1;
+
+        questions[4].var_answers = "4) how many bytes long contains : \n1 - 2 bytes   2 - 4 bytes\n\n";
+        questions[4].answer = 1;
+
+        questions[5].var_answers = "4) how many bytes float contains : \n1 - 4 bytes   2 - 16 bytes\n\n";
+        questions[5].answer = 1;
+
+    }
+    void res(){
+
+        for (int i = 0; i < count_of_questions; ++i)
+        {
+            cout << questions[i].question;
+            cout << questions[i].var_answers;
+            int ans;
+            cin >> ans;
+            if (questions[i].check_answer(ans)) ++right;
+            else ++wrong;
+        }       
+        cout << "\nRight answers - " << right;
+        cout << "\nWrong answers - " << wrong;
+        mark = mark / count_of_questions;
+        for (int i = 0; i < count_of_questions; i++)
+            res = right * mark;
+        cout << "\nYour GPA is " << res << "\n";
+    }
+   
+
+
+
+};
+int menu_user() {
+    cout << "\n1 - Test programing ";
+    cout << "\n2 - Test math ";
+    cout << "\n3 - Test deigner ";
+    cout << "\n4 - Test administration ";
+    cout << "\nEnter your choice :";
+    int choice2;
+    cin >> choice2;
+    return choice2;
+
+
+}
+
+class LogPass{
+    string Login;
+    string Password;
 public:
-    void login_user() {
-        if (choice == 1) {
-            while (choice) {
-                cout << "\n\n";
-                cout << "\t----------------" << " Registeration User " << "----------------" << endl;
-                cout << "Login: ";
-                cin >> user;
-                cout << "password: ";
-                cin >> password;
-
-                cout << "\n\n";
-                cout << "Registered Successfully!" << "\n";
-                cout << "\n\n";
-
-                while (choice) {
-                    cout << "\n\n";
-                    cout << "\t----------- " << "Login" << " " << " -----------";
-                    cout << "\n";
-
-                    cout << "login: ";
-                    cin >> login;
-                    cout << "Password: ";
-                    cin >> password_login;
-
-                    if (login == user && password_login == password) {
-                        cout << "\n" << "loged in Successfully!" << "\n";
-                        cout << "Welcome  " << user << "!!!";
-                        break;
-                    }
-                    else if (login != user && password_login == password) {
-                        cout << "username is incorrect!" << endl;
-                    }
-                    else if (login == user && password_login != password) {
-                        cout << "password is incorrect!" << endl;
-                    }
-                    else {
-                        cout << "Everything is incorrect!" << endl;
-                    }
-                }while (choice);
-            }
-        }
-        else {
-            while (choice == 2) {
-                cout << "\n\n";
-                cout << "\t----------------" << " Registeration Admin" << "----------------" << endl;
-                cout << "Login: ";
-                cin >> user;
-                cout << "password: ";
-                cin >> password;
-
-                cout << "\n\n";
-                cout << "Registered Successfully!" << "\n";
-                cout << "\n\n";
-
-                while (choice) {
-                    cout << "\n\n";
-                    cout << "\t----------- " << "Login" << " " << " -----------";
-                    cout << "\n";
-
-                    cout << "login: ";
-                    cin >> login;
-                    cout << "Password: ";
-                    cin >> password_login;
-
-                    if (login == user && password_login == password) {
-                        cout << "\n" << "loged in Successfully!" << "\n";
-                        cout << "WELCOME  " << user << "!!!";
-                        break;
-                    }
-                    else if (login != user && password_login == password) {
-                        cout << "username is incorrect!" << endl;
-                    }
-                    else if (login == user && password_login != password) {
-                        cout << "password is incorrect!" << endl;
-                    }
-                    else {
-                        cout << "Everything is incorrect!" << endl;
-                    }
-                }while (choice);
-            }
-        }
-
+    LogPass(string L, string P)
+    {
+        Login = L;
+        Password = P;
+    }
+    bool acces(LogPass l) {
+        return  (Login == l.Login && Password == l.Password);
     }
 };
 
 int main() {
-    login l;
-    l.login_user();
+      tests t;
+      string Log = "user";
+      string Pass = " 1";
+      string login, password;
+      int choice = menu();
+      switch (choice) {
+      case 1:
+          cout << "\t----------------" << " Login " << "----------------" << "\n";
+          cout << "Login  ";
+          cin >> Log;
+          cout << "Password  ";
+          cin >> Pass;
+          if (login == Log && Pass == password) {
+              cout << "\n" << "loged in Successfully!" << "\n";
+              cout << "WELCOME  " << login << "!!!";
+          }
+          break;
+      }
+
+      int choice2 = menu_user();
+      switch (choice2) {
+      case 1:
+          t.test_programing();
+       
+          break;
+      case 2:
+      
+          break;
 
 
 
-
+      }
 
     return 0;
 }
