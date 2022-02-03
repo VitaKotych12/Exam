2e12391b52b1be22ead10ad80fac6dc354515e57
Vita <=>
2022-02-03 21:11:39 +0200

    03.02.2022

--- Exam/Source.cpp
+++ Exam/Source.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+﻿#include<iostream>
 #include<string>
 #include<map>
 #include<fstream>
@@ -15,25 +15,6 @@ int menu() {
 	cin >> log;
 	return log;
 }
-class Guest {
-	string login, password, user;
-public : 
-	Guest() { login = password =" "; }
-	Guest(string login, string password) {
-		this->login = login;
-		this->password = password;
-	}
- 
-    string get_login() {
-        return login;
-    }
-    string get_password() {
-        return password;
-    }
-    string get_user() {
-        return user;
-    }
-};
 
 struct Question
 {
@@ -73,9 +54,6 @@ class tests {
         questions[5].var_answers = "4) how many bytes float contains : \n1 - 4 bytes   2 - 16 bytes\n\n";
         questions[5].answer = 1;
 
-    }
-    void res(){
-
         for (int i = 0; i < count_of_questions; ++i)
         {
             cout << questions[i].question;
@@ -92,7 +70,44 @@ class tests {
             res = right * mark;
         cout << "\nYour GPA is " << res << "\n";
     }
-   
+    void test_math() {
+
+        questions.resize(count_of_questions);
+        questions[0].question = "\nThere are 6  questions in this test : \n";
+        questions[0].var_answers = "1) √(256^2) \n1 - 256   2 - 65536    3 - 512   \n\n";
+        questions[0].answer = 2;
+
+        questions[1].var_answers = "2) 2 + 8 \n1 - 10   2 - 19   3 - 9   4 - 6   \n\n";
+        questions[1].answer = 1;
+
+        questions[2].var_answers = "3) 2 log(8 × 8^8) \n1 - 16   2 - 17   2 - 8   2 - 2\n\n";
+        questions[2].answer = 1;
+
+        questions[3].var_answers = "4) 4^6  \n1 - 4096   2 - 409   3 - 496 \n\n";
+        questions[3].answer = 1;
+
+        questions[4].var_answers = "4) 7^2 \n1 - 70   2 - 64   3 - 49   4 - 7*7\n\n";
+        questions[4].answer = 1;
+
+        questions[5].var_answers = "4) 125^2 \n1 - 15625   2 - 156255   3 - 1525  4 - 5625 \n\n";
+        questions[5].answer = 1;
+
+        for (int i = 0; i < count_of_questions; ++i)
+        {
+            cout << questions[i].question;
+            cout << questions[i].var_answers;
+            int ans;
+            cin >> ans;
+            if (questions[i].check_answer(ans)) ++right;
+            else ++wrong;
+        }
+        cout << "\nRight answers - " << right;
+        cout << "\nWrong answers - " << wrong;
+        mark = mark / count_of_questions;
+        for (int i = 0; i < count_of_questions; i++)
+            res = right * mark;
+        cout << "\nYour GPA is " << res << "\n";
+    }
 
 
 
@@ -119,40 +134,68 @@ public:
         Login = L;
         Password = P;
     }
-    bool acces(LogPass l) {
-        return  (Login == l.Login && Password == l.Password);
+    int acces(LogPass l,LogPass p) {
+        return  (Login == l.Login && Password == p.Password);
     }
 };
 
-int main() {
-      tests t;
-      string Log = "user";
-      string Pass = " 1";
-      string login, password;
-      int choice = menu();
-      switch (choice) {
-      case 1:
-          cout << "\t----------------" << " Login " << "----------------" << "\n";
-          cout << "Login  ";
-          cin >> Log;
-          cout << "Password  ";
-          cin >> Pass;
-          if (login == Log && Pass == password) {
-              cout << "\n" << "loged in Successfully!" << "\n";
-              cout << "WELCOME  " << login << "!!!";
-          }
-          break;
-      }
 
-      int choice2 = menu_user();
-      switch (choice2) {
+int main() {
+    tests t;
+    string login, password;
+    int choice = menu();
+    switch (choice == 1) {
+    case 1:
+        cout << "\t----------------" << " Login User " << "----------------" << "\n";
+        cout << "Login  ";
+        cin >> login;
+        cout << "Password  ";
+        cin >> password;
+        LogPass l(login, password);
+        LogPass user("user", "user");
+        bool  acc = false;
+        acc = user.acces(l, l);
+        if (acc) {
+            cout << "\n" << "loged in Successfully!" << "\n";
+            cout << "WELCOME  " << login << " !";
+        }
+        else {
+            cout << "There is no account with this log ";
+            break;
+        }
+        int choice2 = menu_user();
+        switch (choice2) {
+        case 1:
+            t.test_programing();
+            break;
+        case 2:
+            t.test_math();
+            break;
+
+
+        }
+    }
+    
+
+      switch (choice == 2) {
       case 1:
-          t.test_programing();
-       
-          break;
-      case 2:
-      
-          break;
+          ///////////////////// admin
+          cout << "\t----------------" << " Login Admin " << "----------------" << "\n";
+          cout << "Login  ";
+          cin >> login;
+          cout << "Password  ";
+          cin >> password;
+          LogPass l(login, password);
+          LogPass admin("admin", "admin");
+          bool  acc = false;
+          acc = admin.acces(l, l);
+          if (acc) {
+              cout << "\n" << "loged in Successfully!" << "\n";  cout << "WELCOME  " << login << " !";
+          }
+          else {
+              cout << "There is no account with this log ";
+              break;
+          }
 
 
 
