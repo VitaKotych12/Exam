9398a59bca06b6aea2381f6f93bb0209ae54ae00
Vita <=>
2022-02-05 11:07:46 +0200

    05.02.22

--- Exam/Source.cpp
+++ Exam/Source.cpp
@@ -10,9 +10,9 @@ using namespace std;
 
 struct Question
 {
-    string question = " ", var_answers = " ";  int answer = 0;
+    string question = " ", answers = " ";  int answer = 0;
     bool check_answer(int num)
-    { 
+    {
         return (answer == num);
     }
 };
@@ -23,38 +23,38 @@ class tests {
     int wrong = 0;
     double res = 0;
     int mark = 12;
- public:    
+public:
     void test_programing() {
 
         questions.resize(count_of_questions);
         questions[0].question = "\nThere are 6  questions in this test : \n";
-        questions[0].var_answers = "1) C ++ was created in 1979 : \n1 - yes   2 - no\n\n";
+        questions[0].answers = "1) C ++ was created in 1979 : \n1 - yes   2 - no\n\n";
         questions[0].answer = 2;
 
-        questions[1].var_answers = "2) C ++ is object oriented : \n1 - yes   2 - no\n\n";
+        questions[1].answers = "2) C ++ is object oriented : \n1 - yes   2 - no\n\n";
         questions[1].answer = 1;
 
-        questions[2].var_answers = "3) how many bytes char contains : \n1 - 1 byte   2 - 8 bytes\n\n";
+        questions[2].answers = "3) how many bytes char contains : \n1 - 1 byte   2 - 8 bytes\n\n";
         questions[2].answer = 1;
 
-        questions[3].var_answers = "4) how many bytes int contains :  \n1 - 2 bytes   2 - 16 bytes\n\n";
+        questions[3].answers = "4) how many bytes int contains :  \n1 - 2 bytes   2 - 16 bytes\n\n";
         questions[3].answer = 2;
 
-        questions[4].var_answers = "4) how many bytes long contains : \n1 - 2 bytes   2 - 4 bytes\n\n";
+        questions[4].answers = "4) how many bytes long contains : \n1 - 2 bytes   2 - 4 bytes\n\n";
         questions[4].answer = 1;
 
-        questions[5].var_answers = "4) how many bytes float contains : \n1 - 4 bytes   2 - 16 bytes\n\n";
+        questions[5].answers = "4) how many bytes float contains : \n1 - 4 bytes   2 - 16 bytes\n\n";
         questions[5].answer = 1;
 
         for (int i = 0; i < count_of_questions; ++i)
         {
             cout << questions[i].question;
-            cout << questions[i].var_answers;
+            cout << questions[i].answers;
             int ans;
             cin >> ans;
             if (questions[i].check_answer(ans)) ++right;
             else ++wrong;
-        }       
+        }
         cout << "\nRight answers - " << right;
         cout << "\nWrong answers - " << wrong;
         mark = mark / count_of_questions;
@@ -68,28 +68,28 @@ class tests {
 
         questions.resize(count_of_questions);
         questions[0].question = "\nThere are 6  questions in this test : \n";
-        questions[0].var_answers = "1) C ++ was created in 1979 : \n1 - yes   2 - no\n\n";
+        questions[0].answers = "1) C ++ was created in 1979 : \n1 - yes   2 - no\n\n";
         questions[0].answer = 2;
 
-        questions[1].var_answers = "2) C ++ is object oriented : \n1 - yes   2 - no\n\n";
+        questions[1].answers = "2) C ++ is object oriented : \n1 - yes   2 - no\n\n";
         questions[1].answer = 1;
 
-        questions[2].var_answers = "3) how many bytes char contains : \n1 - 1 byte   2 - 8 bytes\n\n";
+        questions[2].answers = "3) how many bytes char contains : \n1 - 1 byte   2 - 8 bytes\n\n";
         questions[2].answer = 1;
 
-        questions[3].var_answers = "4) how many bytes int contains :  \n1 - 2 bytes   2 - 16 bytes\n\n";
+        questions[3].answers = "4) how many bytes int contains :  \n1 - 2 bytes   2 - 16 bytes\n\n";
         questions[3].answer = 2;
 
-        questions[4].var_answers = "4) how many bytes long contains : \n1 - 2 bytes   2 - 4 bytes\n\n";
+        questions[4].answers = "4) how many bytes long contains : \n1 - 2 bytes   2 - 4 bytes\n\n";
         questions[4].answer = 1;
 
-        questions[5].var_answers = "4) how many bytes float contains : \n1 - 4 bytes   2 - 16 bytes\n\n";
+        questions[5].answers = "4) how many bytes float contains : \n1 - 4 bytes   2 - 16 bytes\n\n";
         questions[5].answer = 1;
 
         for (int i = 0; i < count_of_questions; ++i)
         {
             cout << questions[i].question;
-            cout << questions[i].var_answers;
+            cout << questions[i].answers;
             int ans;
             cin >> ans;
             if (questions[i].check_answer(ans)) ++right;
@@ -108,29 +108,29 @@ class tests {
 
         questions.resize(count_of_questions);
         questions[0].question = "\nThere are 6  questions in this test : \n";
-        questions[0].var_answers = "1) 2+ 2 \n1 - 4   2 - 5    3 - 22   \n\n";
+        questions[0].answers = "1) 2+ 2 \n1 - 4   2 - 5    3 - 22   \n\n";
         questions[0].answer = 1;
 
-        questions[1].var_answers = "2) 2 + 8 \n1 - 10   2 - 19   3 - 9   4 - 6   \n\n";
+        questions[1].answers = "2) 2 + 8 \n1 - 10   2 - 19   3 - 9   4 - 6   \n\n";
         questions[1].answer = 2;
 
-        questions[2].var_answers = "3) 2*2 \n1 - 16   2 - 17   3 - 4   4 - 2\n\n";
+        questions[2].answers = "3) 2*2 \n1 - 16   2 - 17   3 - 4   4 - 2\n\n";
         questions[2].answer = 3;
 
-        questions[3].var_answers = "4) 4+6  \n1 - 96   2 - 4   3 - 10 \n\n";
+        questions[3].answers = "4) 4+6  \n1 - 96   2 - 4   3 - 10 \n\n";
         questions[3].answer = 3;
 
-        questions[4].var_answers = "4) 72-2 \n1 - 70   2 - 64   3 - 49   4 - 7*7\n\n";
+        questions[4].answers = "4) 72-2 \n1 - 70   2 - 64   3 - 49   4 - 7*7\n\n";
         questions[4].answer = 1;
 
-        questions[5].var_answers = "4) 125-100 \n1 - 25   2 - 0   3 - 100  4 - 4 \n\n";
+        questions[5].answers = "4) 125-100 \n1 - 25   2 - 0   3 - 100  4 - 4 \n\n";
         questions[5].answer = 1;
 
 
         for (int i = 0; i < count_of_questions; ++i)
         {
             cout << questions[i].question;
-            cout << questions[i].var_answers;
+            cout << questions[i].answers;
             int ans;
             cin >> ans;
             if (questions[i].check_answer(ans)) ++right;
@@ -147,31 +147,31 @@ class tests {
     }
     void test_high_math() {
 
-        questions.resize(count_of_questions);
-        questions[0].question = "\nThere are 6  questions in this test : \n";
-        questions[0].var_answers = "1) √(256^2) \n1 - 256   2 - 65536    3 - 512   \n\n";
+      /*  questions.resize(count_of_questions);
+
+        questions[0].answers = "1) √(256^2) \n1 - 256   2 - 65536    3 - 512   \n\n";
         questions[0].answer = 1;
 
-        questions[1].var_answers = "2) 2 + 8 \n1 - 10   2 - 19   3 - 9   4 - 6   \n\n";
+        questions[1].answers = "2) 2 + 8 \n1 - 10   2 - 19   3 - 9   4 - 6   \n\n";
         questions[1].answer = 1;
 
-        questions[2].var_answers = "3) 2 log(8 × 8^8) \n1 - 16   2 - 17   3 - 8   4 - 2\n\n";
-        questions[2].answer = 1;
+        questions[2].answers = "3) 2 log(8 × 8^8) \n1 - 16   2 - 17   3 - 8   4 - 2\n\n";
+        questions[2].answer = 1;*/
 
-        questions[3].var_answers = "4) 4^6  \n1 - 4096   2 - 409   3 - 496 \n\n";
+        questions[3].answers = "4) 4^6  \n1 - 4096   2 - 409   3 - 496 \n\n";
         questions[3].answer = 1;
 
-        questions[4].var_answers = "4) 7^2 \n1 - 70   2 - 64   3 - 49   4 - 7*7\n\n";
+        questions[4].answers = "4) 7^2 \n1 - 70   2 - 64   3 - 49   4 - 7*7\n\n";
         questions[4].answer = 3;
 
-        questions[5].var_answers = "4) 125^2 \n1 - 15625   2 - 156255   3 - 1525  4 - 5625 \n\n";
+        questions[5].answers = "4) 125^2 \n1 - 15625   2 - 156255   3 - 1525  4 - 5625 \n\n";
         questions[5].answer = 1;
 
 
         for (int i = 0; i < count_of_questions; ++i)
         {
             cout << questions[i].question;
-            cout << questions[i].var_answers;
+            cout << questions[i].answers;
             int ans;
             cin >> ans;
             if (questions[i].check_answer(ans)) ++right;
@@ -187,32 +187,32 @@ class tests {
         right = 0; wrong = 0; res = 0;  mark = 12;
     }
     void test_elementary_films() {
-       
+
         questions.resize(count_of_questions);
         questions[0].question = "\nThere are 6  questions in this test : \n";
-        questions[0].var_answers = "1) Do you like watching films  \n1 - yes   2 - no  \n\n";
+        questions[0].answers = "1) Do you like watching films  \n1 - yes   2 - no  \n\n";
         questions[0].answer = 1;
 
-        questions[1].var_answers = "2) A film, also called a movie  \n1 - yes  2- no  \n\n";
+        questions[1].answers = "2) A film, also called a movie  \n1 - yes  2- no  \n\n";
         questions[1].answer = 1;
 
-        questions[2].var_answers = "3) The stroboscopic animation principle was introduced in 1833  \n1 - yes   2 - no\n\n";
+        questions[2].answers = "3) The stroboscopic animation principle was introduced in 1833  \n1 - yes   2 - no\n\n";
         questions[2].answer = 1;
 
-        questions[3].var_answers = "4) Photography was introduced in 1839  \n1 - yes 2- no \n\n";
+        questions[3].answers = "4) Photography was introduced in 1839  \n1 - yes 2- no \n\n";
         questions[3].answer = 1;
 
-        questions[4].var_answers = "4)  Around the turn of the 20th century,films started stringing several scenes together to tell a story. \n1 - yes  2 - no\n\n";
+        questions[4].answers = "4)  Around the turn of the 20th century,films started stringing several scenes together to tell a story. \n1 - yes  2 - no\n\n";
         questions[4].answer = 1;
 
-        questions[5].var_answers = "4) Digital 3D projection largely replaced earlier problem-prone 3D film systems and has become popular in the early 2010s \n1 - yes   2 - no \n\n";
+        questions[5].answers = "4) Digital 3D projection largely replaced earlier problem-prone 3D film systems and has become popular in the early 2010s \n1 - yes   2 - no \n\n";
         questions[5].answer = 1;
-        
-       
+
+
         for (int i = 0; i < count_of_questions; ++i)
         {
             cout << questions[i].question;
-            cout << questions[i].var_answers;
+            cout << questions[i].answers;
             int ans;
             cin >> ans;
             if (questions[i].check_answer(ans)) ++right;
@@ -231,29 +231,29 @@ class tests {
 
         questions.resize(count_of_questions);
         questions[0].question = "\nThere are 6  questions in this test about Angelina Jolie : \n";
-        questions[0].var_answers = "1)Ahe is an amerian singer , isn't it \n1 - yes 2 - no   \n\n";
+        questions[0].answers = "1)Ahe is an amerian singer , isn't it \n1 - yes 2 - no   \n\n";
         questions[0].answer = 1;
 
-        questions[1].var_answers = "2) How old she is  \n1 - 10   2 - 19   3 - 89   4 - 46   \n\n";
+        questions[1].answers = "2) How old she is  \n1 - 10   2 - 19   3 - 89   4 - 46   \n\n";
         questions[1].answer = 4;
 
-        questions[2].var_answers = "3) Where does she live \n1 - New York   2 - Los angeles  2 - I don't know   2 - I really don't know\n\n";
+        questions[2].answers = "3) Where does she live \n1 - New York   2 - Los angeles  2 - I don't know   2 - I really don't know\n\n";
         questions[2].answer = 2;
 
-        questions[3].var_answers = "4) Has she got an oscar \n1 - yes  2 - no \n\n";
+        questions[3].answers = "4) Has she got an oscar \n1 - yes  2 - no \n\n";
         questions[3].answer = 1;
-         
-        questions[4].var_answers = "4) Her high is \n1 - 1,70   2 - 1,64   3 - 1,49   4 - 1,69\n\n";
+
+        questions[4].answers = "4) Her high is \n1 - 1,70   2 - 1,64   3 - 1,49   4 - 1,69\n\n";
         questions[4].answer = 4;
 
-        questions[5].var_answers = "4) She has been married 3 times \n1 - yes   2 - no   3 - 2 times  4 - once \n\n";
+        questions[5].answers = "4) She has been married 3 times \n1 - yes   2 - no   3 - 2 times  4 - once \n\n";
         questions[5].answer = 1;
 
 
         for (int i = 0; i < count_of_questions; ++i)
         {
             cout << questions[i].question;
-            cout << questions[i].var_answers;
+            cout << questions[i].answers;
             int ans;
             cin >> ans;
             if (questions[i].check_answer(ans)) ++right;
@@ -303,17 +303,9 @@ public:
 
 };
 class admin {
-    map<string,list<guest>> m;
-public: 
-   /* void add_new_user(string login, string password)
-    {
-        auto it = m.find(login);
-        if (it != m.end())
-        {
-            it->second.push_back(guest(password));
-        }
-        m.insert({ login , list<guest>{guest(password)} });
-    }*/
+    map<string, list<guest>> m;
+public:
+     
 
     void remove_login(string login) {
         if (m.find(login) == m.end()) {
@@ -331,18 +323,18 @@ public:
 
 };
 
-class LogPass{
+class LogPass {
     string Login;
     string Password;
 public:
-   
+
     LogPass(string L, string P)
     {
         Login = L;
         Password = P;
     }
     void login_user() {
-        ofstream fout("Registration.txt",ios::app); /* создание потока fout и открытие файла с именем massiv.dat для записи */
+        ofstream fout("Registration.txt", ios::app); 
 
         if (!fout) cout << "Cannot open file\n";
         else {
@@ -356,12 +348,12 @@ public:
             cout << "\n\n";
             cout << "Registered Successfully!" << "\n";
             cout << "\n\n";
-            fout << " " << Login <<"  "<< Password;;
+            fout << " " << Login << "  " << Password;;
         }
         fout.close();
         string line;
 
-       ifstream in("Registration.txt"); // окрываем файл для чтения
+        ifstream in("Registration.txt"); 
         if (in.is_open())
         {
             while (getline(in, line))
@@ -374,10 +366,12 @@ public:
         }
         in.close();
     }
-    int acces(LogPass l,LogPass p) {
+    int acces(LogPass l, LogPass p) {
         return  (Login == l.Login && Password == p.Password);
     }
 };
+
+
 
 int menu1() {
     int log;
@@ -420,30 +414,36 @@ int menu_admin() {
 }
 
 int main() {
+    map<string, list<guest>> m;
     tests t; string login, password; LogPass l(login, password);
     LogPass user(login, password);
     bool  acc = false;
     acc = user.acces(l, l);
 
-    switch (menu1()) {
-       case 1:
+    switch (true) {
+    case 1:
         l.login_user();
         cout << "\t----------------" << " Login User " << "----------------" << "\n";
         cout << "Login  ";
         cin >> login;
         cout << "Password  ";
         cin >> password;
-     
-        if (acc) {
+
+        if (acc == true) {
             cout << "\n" << "loged in Successfully!" << "\n";
             cout << "WELCOME  " << login << " !";
         }
         else {
             cout << "There is no account with this log ";
             break;
-        }
-   
+        }   
+        case 0:
+            menu1();
+
         switch (menu2()) {
+        case 0:
+            menu1();
+
         case 1:
             ////////////////////////////////////////
             switch (menu3()) {
@@ -479,9 +479,9 @@ int main() {
             }
             menu2();
         }
-    
+
     }
-    switch (menu1() == 2) {
+    switch (true) {
     case 1:
         ///////////////////// admin
         admin a; string delete_log;
@@ -506,71 +506,26 @@ int main() {
         case 0:
             menu2();
         case 1:
+            cout << "Enter login : ";
+            cin >> login;
+            cout << "Enter password : ";
+            cin >> password;
+            //a.add_new_user(login, password);
             break;
         case 2:
             cout << "Enter login which you want to delete : ";
             cin >> delete_log;
             a.remove_login(delete_log);
-            break;
-
-
-
-        }
-
-
-        }
-        ////////////////////////////////////////
-
-        //int choice2 = menu_user();
-        //switch (choice2) {
-        //case 0:
-        //    choice = menu();
-        //    break;
-        //case 1:
-        //    ////////////////////////////////////////
-        //    switch (menu3()) {
-        //    case 0:
-        //       return choice = menu_user();
-        //    case 1:
-        //        t.test_programing();
-        //        break;
-        //    case 2:
-        //        t.test_All_programing();
-        //        break;
-        //    }
-        //    ///////////////////////////////////////
-        //  choice2 = menu_user();
-        //case 2:
-        //    switch (menu4()) {
-        //    case 0:
-        //        return choice = menu_user();
-        //    case 1:
-        //        t.test_elementary_math();
-        //        break;
-        //    case 2:
-        //        t.test_high_math();
-        //        break;
-        //    }
-        //    break;
-        //    ///////////////////////////////////////
-
-       /* case 3:
-            switch (menu5()) {
-            case 0:
-                choice = menu_user();
                 break;
-            case 1:
-                t.test_high_math();
-                break;
-            case 2:
-                t.test_high_math();
-                break;
-            }*/
-    
-    
-  
+
+
+
+        }while (menu1() == 2);
+
+
+    }
+
+
 
     return 0;
 }
-
-
