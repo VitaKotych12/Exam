f67c39a64a7845268752d90e36959d1b89f65638
Vita <=>
2022-02-06 21:50:36 +0200

    06.02.22

--- Exam/Source.cpp
+++ Exam/Source.cpp
@@ -8,6 +8,45 @@
 #include<list>
 using namespace std;
 
+int menu1() {
+    int log;
+    cout << "\n0 - Exit";
+    cout << "\n1 - Guest ";
+    cout << "\n2 - Admin\n";
+    cout << "Enter who are you : ";
+    cin >> log;
+    return log;
+}
+int menu2() {
+    cout << "\n\t0 - Exit ";
+    cout << "\n\t1 - Test Programing ";
+    cout << "\n\t2 - Test Math ";
+    cout << "\n\t3 - Test Films ";
+    cout << "\\tEnter your choice :";
+    int choice2;
+    cin >> choice2;
+    return choice2;
+}
+int menu3() {
+    int choice3;
+    cout << "\n\t\t0 - Exit ";
+    cout << "\n\t\t1 - Elementary ";
+    cout << "\n\t\t2 - High level ";
+    cout << "\n\t\tEnter your choice :  ";
+    cin >> choice3;
+    return choice3;
+}
+int menu_admin() {
+    int choice;
+    cout << "\n\t0 - Exit \n";
+    cout << "\t1 - Add new profile  \n";
+    cout << "\t2 - Delete profile  \n";
+    cout << "\t3 - Add new task \n";
+    cout << "Enter your choice : ";
+    cin >> choice;
+    return choice;
+
+}
 struct Question
 {
     string question = " ", answers = " ";  int answer = 0;
@@ -25,6 +64,7 @@ class tests {
     int mark = 12;
 public:
     void test_programing() {
+
 
         questions.resize(count_of_questions);
         questions[0].question = "\nThere are 6  questions in this test : \n";
@@ -147,7 +187,7 @@ public:
     }
     void test_high_math() {
 
-      /*  questions.resize(count_of_questions);
+        questions.resize(count_of_questions);
 
         questions[0].answers = "1) √(256^2) \n1 - 256   2 - 65536    3 - 512   \n\n";
         questions[0].answer = 1;
@@ -156,7 +196,7 @@ public:
         questions[1].answer = 1;
 
         questions[2].answers = "3) 2 log(8 × 8^8) \n1 - 16   2 - 17   3 - 8   4 - 2\n\n";
-        questions[2].answer = 1;*/
+        questions[2].answer = 1;
 
         questions[3].answers = "4) 4^6  \n1 - 4096   2 - 409   3 - 496 \n\n";
         questions[3].answer = 1;
@@ -275,6 +315,7 @@ public:
 };
 class guest {
     string password, login;
+    map<string, list<guest> > m ;
 public:
     guest() { login = password = " "; }
 
@@ -305,8 +346,6 @@ public:
 class admin {
     map<string, list<guest>> m;
 public:
-     
-
     void remove_login(string login) {
         if (m.find(login) == m.end()) {
             cout << "Not Found\n";
@@ -318,7 +357,24 @@ public:
             cout << "Delete\n";
         };
     }
+    void add_new_test() {
+        string test, questions;
+        cout << "Enter test name : ";
+        cin >> test;
+        cout << "Do you want to add questions for test :\n 1 - yes  2 - no  ";
+        int yes;
+        cin >> yes;
+        if (yes) {
+            cout << "Enter how many quetstions you want : ";
+            int count;
+            cin >> count;
+            for (int i = 0; i < count; i++) {
+                cout << "Enter quetion number  " << i << " :\n";
+                cin >> questions;
 
+            }
+        }
+    }
 
 
 };
@@ -346,9 +402,10 @@ public:
             cin >> Password;
 
             cout << "\n\n";
+            cout << "\n\n";
             cout << "Registered Successfully!" << "\n";
             cout << "\n\n";
-            fout << " " << Login << "  " << Password;;
+            fout << " " << Login << "  " << Password<<"  \n";
         }
         fout.close();
         string line;
@@ -358,7 +415,7 @@ public:
         {
             while (getline(in, line))
             {
-                cout << line << "\n";
+                cout << line << "\n\n\n";
             }
         }
         else {
@@ -373,49 +430,12 @@ public:
 
 
 
-int menu1() {
-    int log;
-    cout << "\n0 - Exit";
-    cout << "\n1 - Guest ";
-    cout << "\n2 - Admin\n";
-    cout << "Enter who are you : ";
-    cin >> log;
-    return log;
-}
-int menu2() {
-    cout << "\n0 - Exit ";
-    cout << "\n1 - Test Programing ";
-    cout << "\n2 - Test Math ";
-    cout << "\n3 - Test Films ";
-    cout << "\nEnter your choice :";
-    int choice2;
-    cin >> choice2;
-    return choice2;
-}
-int menu3() {
-    int choice3;
-    cout << "\n\t0 - Exit ";
-    cout << "\n\t1 - Elementary ";
-    cout << "\n\t2 - High level ";
-    cout << "\n\tEnter your choice :  ";
-    cin >> choice3;
-    return choice3;
-}
-int menu_admin() {
-    int choice;
-    cout << "\t0 - Exit \n";
-    cout << "\t1 - Add new profile  \n";
-    cout << "\t2 - Delete profile  \n";
-    cout << "\t3 - Exit \n";
-    cout << "Enter your choice : ";
-    cin >> choice;
-    return choice;
-
-}
 
 int main() {
-    map<string, list<guest>> m;
-    tests t; string login, password; LogPass l(login, password);
+   
+    tests t; 
+    string login, password;
+    LogPass l(login, password);
     LogPass user(login, password);
     bool  acc = false;
     acc = user.acces(l, l);
@@ -425,7 +445,7 @@ int main() {
         l.login_user();
         cout << "\t----------------" << " Login User " << "----------------" << "\n";
         cout << "Login  ";
-        cin >> login; 
+        cin >> login;
         cout << "Password  ";
         cin >> password;
 
@@ -436,55 +456,51 @@ int main() {
         else {
             cout << "There is no account with this log ";
             break;
-        }   
-        case 0:
-            menu1();
-
-        switch (menu2()) {
-        case 0:
-            menu1();
-
+        }
+        switch (menu2()==1) {
+            if (!menu2())break;
         case 1:
             ////////////////////////////////////////
-            switch (menu3()) {
-            case 0:
-                menu1();
-            case 1:
-                t.test_programing();
-                break;
-            case 2:
-                t.test_All_programing(); break;
-            }
-            menu2();
+            do {
+                if (!menu2())break;
+                switch (menu3()) {
+                case 1:
+                    t.test_programing(); break;
+                case 2:
+                    t.test_All_programing(); break;
+                }break;
+            } while (true);
         case 2:
             ////////////////////////////////////////
-            switch (menu3()) {
-            case 0:
-                menu1();
-            case 1:
-                t.test_elementary_math(); break;
-            case 2:
-                t.test_high_math(); break;
-            }
-            menu2();
+            do {
+                if (!menu2())break;
+                switch (menu3()) {
+                case 1:
+                    t.test_elementary_math(); break;
+                case 2:
+                    t.test_high_math(); break;
+                }break;
+            } while (true);
         case 3:
             ////////////////////////////////////////
-            switch (menu3()) {
-            case 0:
-                menu1();
-            case 1:
-                t.test_elementary_films(); break;
-            case 2:
-                t.test_high_films();  break;
-            }
-            menu2();
+            do {
+                if (!menu2())break;
+                switch (menu3()) {
+                case 1:
+                    t.test_elementary_films(); break;
+                case 2:
+                    t.test_high_films();  break;
+                }break;
+            } while (true);
         }
-
+    
     }
-    switch (true) {
+
+    switch (menu1() == 2) {
     case 1:
         ///////////////////// admin
         admin a; string delete_log;
+        LogPass p("2", "2");
 
         l.login_user();
         cout << "\t----------------" << " Login Admin " << "----------------" << "\n";
@@ -494,12 +510,12 @@ int main() {
         cin >> password;
         LogPass admin(login, password);
         bool  acc = false;
-        acc = admin.acces(l, l);
+        acc = admin.acces(p, p);
         if (acc) {
             cout << "\n" << "loged in Successfully!" << "\n";  cout << "WELCOME  " << login << " !";
         }
         else {
-            cout << "There is no account with this log ";
+            cout << "There is no account with this login ";
             break;
         }
         switch (menu_admin()) {
@@ -510,15 +526,15 @@ int main() {
             cin >> login;
             cout << "Enter password : ";
             cin >> password;
-            //a.add_new_user(login, password);
             break;
         case 2:
             cout << "Enter login which you want to delete : ";
             cin >> delete_log;
             a.remove_login(delete_log);
                 break;
-
-
+        case 3: 
+            a.add_new_test();
+            break;
 
         }while (menu1() == 2);
 
